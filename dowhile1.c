4deba2a770b0d6b198571af7242721954ab3ff4c
Lcesarbp <106997797+Lcesarbp@users.noreply.github.com>
2025-02-06 15:47:58 +0000

    inseriu comentário

--- dowhile1.c
+++ dowhile1.c
@@ -4,6 +4,8 @@ int numsecret = 8;
 int num = 0;
 
     do {
+    
+    //Do while é para ser usado quando se precisa testar a condição pelo menos uma vez: No caso já vai começar fazendo o que está pedindo no comando "Do" o if testa a condição do numero digitado com o numero secreto ele vai fazer a repetição enquento o teste for verdadeiro
 
     printf("Adivinhe o número secreto: ");
     scanf("%d", &num);

52bbbcc51840a4516e6b37927718c676ca6462fd
Lcesarbp <106997797+Lcesarbp@users.noreply.github.com>
2025-02-07 00:00:18 +0000

    Inserido while 3

---
+++ while2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int num = 11;
+    int resultado = 0;
+
+while (num >= 1)
+{
+    resultado = num - 1;
+
+    printf("%d ", resultado);
+
+    num --;
+}
+
+printf("FIM!!! \n");
+
+
+return 0;
+
+
+
+}
